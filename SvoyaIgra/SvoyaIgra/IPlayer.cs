ea36e8c4c6dc904424bd1a3d06a599f347dfc8e7
Andrew <kapitansuly2014@yandex.ru>
2019-10-22 20:28:27 +0300

    start project x5

---
+++ SvoyaIgra/SvoyaIgra/IPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SvoyaIgra
+{
+    public interface IPlayer
+    {
+        string Name { get; set; }
+        int Score { get; set; }
+    }
+}
---
+++ SvoyaIgra/SvoyaIgra/QuestionType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SvoyaIgra
+{
+    public enum QuestionType
+    {
+        FreeAnswer,
+        AnswerSelection
+    }
+}
