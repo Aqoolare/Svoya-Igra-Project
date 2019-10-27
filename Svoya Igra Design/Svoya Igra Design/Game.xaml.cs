85776fd26c48d545b52d5d3860a780c3ef57185e
Nikolay <rebelmedved@gmail.com>
2019-10-27 14:29:19 +0300

    Начал делать окно Game

---
+++ Svoya Igra Design/Svoya Igra Design/Game.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SvoyaIgra;
+
+namespace Svoya_Igra_Design
+{
+    /// <summary>
+    /// Логика взаимодействия для Game.xaml
+    /// </summary>
+    public partial class Game : Window
+    {
+        private Config _configuration;
+        public Game(Config Configuration)
+        {
+            InitializeComponent();
+            _configuration = Configuration;
+            StartGame();
+        }
+        public void StartGame();
+        {
+            
+        }
+    }
+}
